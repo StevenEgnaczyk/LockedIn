799e5fc2b0a967f8094fda2f5dc05f079d01e98b
Adam3Haikal3 <haikal.8@osu.edu>
2024-10-19 10:30:43 -0400

    Firebase set-up

--- src/index.js
+++ src/index.js
@@ -4,6 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import stylesheets
+import './style.css';
+
+// Firebase App (the core Firebase SDK) is always required
+import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/app';
+
+// Add the Firebase products and methods that you want to use
+import {} from 'firebase/auth';
+import {} from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+import * as firebaseui from 'firebaseui';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+const analytics = getAnalytics(app);
+const app = firebase.initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
