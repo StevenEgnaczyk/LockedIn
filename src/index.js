6fa3252617c436f15d07328d54fb424de06c1053
Adam3Haikal3 <haikal.8@osu.edu>
2024-10-19 11:21:48 -0400

    added ui dependency

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Import stylesheets
-import './style.css';
-
 // Firebase App (the core Firebase SDK) is always required
 import { initializeApp } from 'firebase/app';
-import firebase from 'firebase/app';
 
 // Add the Firebase products and methods that you want to use
-import {} from 'firebase/auth';
-import {} from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
 import * as firebaseui from 'firebaseui';
 
@@ -24,11 +20,14 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const app = firebase.initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
