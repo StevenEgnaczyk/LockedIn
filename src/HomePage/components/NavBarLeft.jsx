debd5e81fbdcea0048d0ed7fb8dbc27d50d9311f
Kellen <kellen.gary@gmail.com>
2024-10-19 12:36:02 -0400

    navbarleft

--- src/HomePage/components/NavBarLeft.jsx
+++ src/HomePage/components/NavBarLeft.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {Button} from "@nextui-org/react";
-import { BsChevronDoubleRight } from "react-icons/bs";
+import { BsChevronDoubleRight, BsChevronDoubleLeft } from "react-icons/bs";
 
 import './NavBarLeft.css'
 
 const NavBarLeft = () => {
+
+    const [isOpen, setOpen] = useState('false');
+
+    const openNavbar = () => {
+        setOpen(!isOpen);
+    }
+
     return (
-        <div>
-            <Button className={"popout-button"} isIconOnly color="danger" aria-label="Like">
-                <BsChevronDoubleRight className={'icon'}/>
-            </Button>
+        <div className={"navbar-container"}>
+            {isOpen ? (
+                <Button className={"popout-button"} isIconOnly color="danger" aria-label="Like" disableRipple={true}>
+                    <BsChevronDoubleRight className={'icon'} onClick={openNavbar}/>
+                </Button>
+            ) : (
+                <div className={"open-navbar"}>
+                    <div className={'top-row'}>
+                        <div className={"popout-button"}>
+                            <BsChevronDoubleLeft className={'icon'} onClick={openNavbar}/>
+                        </div>
+                        <form className={"search-bar"}>
+                            <input
+                                className={"search-bar"}
+                                type="text"
+                                placeholder="Search..." // Added placeholder
+                                onFocus={(e) => e.target.placeholder = ''} // Clear placeholder on focus
+                                onBlur={(e) => e.target.placeholder = 'Search...'} // Restore placeholder on blur
+                            />
+                            <button type="button"  className={"search-button"}>🔍</button>
+                        </form>
+                    </div>
+
+                </div>
+
+            )};
+
         </div>
     );
 }
