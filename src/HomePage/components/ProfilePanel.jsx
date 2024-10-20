39fae81a97342c544ed69995436da3afcb073c41
Adam3Haikal3 <haikal.8@osu.edu>
2024-10-20 00:19:26 -0400

    out of scope feature

--- src/HomePage/components/ProfilePanel.jsx
+++ src/HomePage/components/ProfilePanel.jsx
@@ -11,21 +11,6 @@ const ProfilePanel = ({node, closeProfile, allNodes}) => {
             <h1>{node.name}</h1>
             {node.company && <h3>Company: {node.company}</h3>}
             {node.position && <h3>Position: {node.position}</h3>}
-            <p>You may know:
-                {node.connections.length > 0 ? (
-                    node.connections.map((connectionId, index) => {
-                        // Find the connected node by ID
-                        const connectedNode = allNodes.find(n => n.id === connectionId);
-                        return connectedNode ? (
-                            <span key={connectionId}>
-            {connectedNode.name}{index < node.connections.length - 1 ? ', ' : ''}
-          </span>
-                        ) : null;
-                    })
-                ) : (
-                    <span>No connections</span>
-                )}
-            </p>
             <p>{node.profile_url}</p>
         </div>
     )
