1b73427ed4083c36538f9a547ea28bb16fce6c83
Kellen <kellen.gary@gmail.com>
2024-10-19 22:40:01 -0400

    navbar right change

--- src/HomePage/components/NavBarRight.jsx
+++ src/HomePage/components/NavBarRight.jsx
@@ -1,15 +1,21 @@
 import React, {useRef, useState} from "react";
-import { Button } from "@nextui-org/react";
-import UploadBar from "./UploadBar"; 
-import './NavBarRight.css'; 
+import './NavBarRight.css';
 import { CiSettings } from "react-icons/ci";
 import { BsFileEarmark } from "react-icons/bs";
+import { BsFunnel } from "react-icons/bs";
+import UserMerge from "./UserMerge";
+import FileUpload from "./FileUpload";
 
 const NavBarRight = () => {
     const [isOpen, setOpen] = useState(false);
     const [fullBarPage, setFullBarPage] = useState(null); // Manage the active page
 
-    const toggleNavbar = () => setOpen(!isOpen);
+    const toggleNavbar = () => {
+        if (isOpen) {
+            setFullBarPage(null);
+        }
+        setOpen(!isOpen);
+    }
 
     const setBarPage = (page) => {
         setFullBarPage(page); // Set the active page based on button click
@@ -21,8 +27,8 @@ const NavBarRight = () => {
                 <div className="full-bar">
                     {/* Render content based on the active page */}
                     {fullBarPage === 'home' && <div>Home Content</div>}
-                    {fullBarPage === 'filter' && <div>Filter Content</div>}
-                    {fullBarPage === 'file-upload' && <UploadBar />}
+                    {fullBarPage === 'filter' && <UserMerge />}
+                    {fullBarPage === 'file-upload' && <FileUpload />}
                 </div>
             )}
             <div className="navbar-dropdown">
@@ -30,12 +36,12 @@ const NavBarRight = () => {
                 <CiSettings className="icon-right" />
             </div>
             {isOpen && (
-                <div>
+                <div className={'opened-buttons'}>
                     <div className="home-button" onClick={() => setBarPage('home')}>
                         <BsFileEarmark className="icon" />
                     </div>
                     <div className="filter-button" onClick={() => setBarPage('filter')}>
-                        <BsFileEarmark className="icon" />
+                        <BsFunnel className="icon" />
                     </div>
                     <div className="file-upload-button" onClick={() => setBarPage('file-upload')}>
                         <BsFileEarmark className="icon" />
